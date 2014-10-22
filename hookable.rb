0544f9b5ed16d271e9c9709082370544b484780f
Kyle Daigle <kyle.daigle@github.com>
2014-10-21 21:55:03 -0400

    Add simple secret checking

--- hookable.rb
+++ hookable.rb
@@ -14,12 +14,27 @@ class HookDelivery
   key :payload, String
   timestamps!
 
+  def secrets_match?
+    return false unless ENV["SECRET_TOKEN"]
+
+    signature = "sha1=" + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new("sha1"), ENV["SECRET_TOKEN"], payload)
+    Rack::Utils.secure_compare(signature, headers["X-Hub-Signature"])
+  end
+
+  def headers
+    JSON.parse(super)
+  end
+
+  def payload
+    JSON.parse(super)
+  end
+
   def pretty_headers
-    JSON.pretty_generate(JSON.parse(headers))
+    JSON.pretty_generate(headers)
   end
 
   def pretty_payload
-    JSON.pretty_generate(JSON.parse(payload))
+    JSON.pretty_generate(payload)
   end
 end
 
