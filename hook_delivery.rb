86c940a562d3de153f9977f5e106cbeb4405efa7
Kyle Daigle <kyle.daigle@github.com>
2014-10-21 22:22:23 -0400

    Setup raw_payload for signature checking

--- hook_delivery.rb
+++ hook_delivery.rb
@@ -6,18 +6,22 @@ class HookDelivery
   timestamps!
 
   def secrets_match?
-    return false unless ENV["SECRET_TOKEN"]
+    return false unless ENV["SECRET_TOKEN"] && headers["X-Hub-Signature"]
 
-    signature = "sha1=" + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new("sha1"), ENV["SECRET_TOKEN"], payload)
+    signature = "sha1=" + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new("sha1"), ENV["SECRET_TOKEN"], raw_payload)
     Rack::Utils.secure_compare(signature, headers["X-Hub-Signature"])
   end
 
   def headers
-    JSON.parse(super)
+    JSON.parse(read_attribute(:headers))
   end
 
   def payload
-    JSON.parse(super)
+    JSON.parse(read_attribute(:payload))
+  end
+
+  def raw_payload
+    read_attribute(:payload)
   end
 
   def pretty_headers
